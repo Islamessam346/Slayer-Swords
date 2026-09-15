dd927e9b341fbad6eb5a2c182b94ad2cfe38446f
sSeif3 <seef.wael15122007@gmail.com>
2026-09-15 19:13:31 +0300

    added Important comment

--- src/main/java/net/slayers/slayerswords/SlayerSwords.java
+++ src/main/java/net/slayers/slayerswords/SlayerSwords.java
@@ -7,6 +7,7 @@ import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// Important comment
 public class SlayerSwords implements ModInitializer {
 	public static final String MOD_ID = "slayerswords";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
