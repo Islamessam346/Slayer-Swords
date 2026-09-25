bca48e4eb0a228c272249c91312ac56d1367ec51
sSeif3 <seef.wael15122007@gmail.com>
2026-09-25 23:15:16 +0300

    test

--- src/main/java/net/slayers/slayerswords/datagen/ModRecipeProvider.java
+++ src/main/java/net/slayers/slayerswords/datagen/ModRecipeProvider.java
@@ -54,6 +54,6 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public String getName() {
-        return "TutorialMod Recipes";
+        return "SlayerSwords Recipes";
     }
 }
