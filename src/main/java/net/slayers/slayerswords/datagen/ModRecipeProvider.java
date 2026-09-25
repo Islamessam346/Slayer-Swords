9ff61982f13be37f9f32ee1f86b77d6e5c4e3f3e
sSeif3 <seef.wael15122007@gmail.com>
2026-09-25 23:02:32 +0300

    Added initial Recipe and Smelting method

---
+++ src/main/java/net/slayers/slayerswords/datagen/ModRecipeProvider.java
@@ -0,0 +1,59 @@
+package net.slayers.slayerswords.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.world.item.crafting.CookingBookCategory;
+import net.minecraft.world.level.ItemLike;
+import net.slayers.slayerswords.block.ModBlocks;
+import net.slayers.slayerswords.item.ModItems;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    public ModRecipeProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected RecipeProvider createRecipeProvider(HolderLookup.Provider registries, RecipeOutput output) {
+        return new RecipeProvider(registries, output) {
+            @Override
+            public void buildRecipes() {
+                List<ItemLike> SUN_INGOT_SMELTABLES =  List.of(ModItems.SUN_ORE, ModBlocks.RAW_SUNORE_BLOCK);
+
+                oreSmelting(SUN_INGOT_SMELTABLES, RecipeCategory.MISC, CookingBookCategory.BLOCKS, ModItems.SUN_INGOT, 0.25f, 200, "sun"  );
+                oreBlasting(SUN_INGOT_SMELTABLES, RecipeCategory.MISC, CookingBookCategory.BLOCKS, ModItems.SUN_INGOT, 0.25f, 100, "sun"  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "TutorialMod Recipes";
+    }
+}
