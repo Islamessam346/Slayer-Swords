bcf58edb6e8b801031937552cc6c923f321ee155
sSeif3 <seef.wael15122007@gmail.com>
2026-09-26 23:58:59 +0300

    Added Meteorite, Scarlet, smelting,blasting, and shaped+shapeless

--- src/main/java/net/slayers/slayerswords/datagen/ModRecipeProvider.java
+++ src/main/java/net/slayers/slayerswords/datagen/ModRecipeProvider.java
@@ -25,9 +25,38 @@ public class ModRecipeProvider extends FabricRecipeProvider {
             @Override
             public void buildRecipes() {
                 List<ItemLike> SUN_INGOT_SMELTABLES =  List.of(ModItems.SUN_ORE, ModBlocks.RAW_SUNORE_BLOCK);
+                List<ItemLike> SCARLET_INGOT_SMELTABLES = List.of(ModItems.SCARLET_ORE, ModBlocks.RAW_SCARLETORE_BLOCK);
+                List<ItemLike> METEORITE_CRYSTALS_SMELTABLES = List.of(ModItems.SHINY_METEORITE_DEBRIS, ModBlocks.METEORITE_ORE);
+
+
+
 
                 oreSmelting(SUN_INGOT_SMELTABLES, RecipeCategory.MISC, CookingBookCategory.BLOCKS, ModItems.SUN_INGOT, 0.25f, 200, "sun"  );
                 oreBlasting(SUN_INGOT_SMELTABLES, RecipeCategory.MISC, CookingBookCategory.BLOCKS, ModItems.SUN_INGOT, 0.25f, 100, "sun"  );
+                oreSmelting(SCARLET_INGOT_SMELTABLES, RecipeCategory.MISC, CookingBookCategory.BLOCKS, ModItems.SCARLET_INGOT, 0.25f, 200, "sun"  );
+                oreBlasting(SCARLET_INGOT_SMELTABLES, RecipeCategory.MISC, CookingBookCategory.BLOCKS, ModItems.SCARLET_INGOT, 0.25f, 100, "sun"  );
+                oreSmelting(METEORITE_CRYSTALS_SMELTABLES, RecipeCategory.MISC, CookingBookCategory.BLOCKS, ModItems.METEORITE_CRYSTALS, 0.25f, 200, "sun"  );
+                oreBlasting(METEORITE_CRYSTALS_SMELTABLES, RecipeCategory.MISC, CookingBookCategory.BLOCKS, ModItems.METEORITE_CRYSTALS, 0.25f, 100, "sun"  );
+
+
+                shaped(RecipeCategory.MISC, ModBlocks.METEORITE_ORE)
+                        .pattern("RRR")
+                        .pattern("RRR")
+                        .pattern("RRR")
+                        .define('R', ModItems.SHINY_METEORITE_DEBRIS)
+                        .unlockedBy(getHasName(ModItems.SHINY_METEORITE_DEBRIS), has(ModItems.SHINY_METEORITE_DEBRIS))
+                        .group("meteorite")
+                        .save(output);
+
+                shapeless(RecipeCategory.MISC, ModItems.SHINY_METEORITE_DEBRIS, 9)
+                        .requires(ModBlocks.METEORITE_ORE)
+                        .unlockedBy(getHasName(ModBlocks.METEORITE_ORE), has(ModBlocks.METEORITE_ORE))
+                        .group("meteorite")
+                        .save(output);
+
+
+
+
 
 
 
